affcf97831a22a6ed5484e49f985c27a40093316
Aadarsh Chandanvandan <85423709+aadarsh0022@users.noreply.github.com>
2024-10-20 22:31:14 +0530

    initial setup of bolgs

---
+++ src/conf/conf.ts
@@ -0,0 +1,14 @@
+type Config = {
+  appwriteUrl: string;
+  apperiteProjectId: string;
+  apperiteDatabaseId: string;
+  apperiteCollectionId: string;
+  apperiteBucketId: string;
+};
+const conf: Config = {
+  appwriteUrl: process.env.APPWRITE_URL as string,
+  apperiteProjectId: process.env.APPERITE_PROJECT_ID as string,
+  apperiteDatabaseId: process.env.APPERITE_DATABASE_ID as string,
+  apperiteCollectionId: process.env.APPERITE_COLLECTION_ID as string,
+  apperiteBucketId: process.env.APPERITE_BUCKET_ID as string,
+};
